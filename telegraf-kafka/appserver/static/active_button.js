7ed4f4841a5a07de7bf99412580163b00d6f55c3
Guilhem Marchand <guilhem.marchand@gmail.com>
2019-04-14 12:12:14 +0100

    Version 1.1.17 (#18)
    - fix: Expose units for Zookeeper latency metrics in Overview and entity view
    - feature: Introducing the smart component enablement, which allows enabling / disabling a Kafka component to be visible from the Overview, to be managed via the configuration user interface
    - feature: Expose Zookeeper leader and Broker active controller in Overview dashboard when mono tenancy (environment) detected or selected
    - feature: Configuration checker, detect incomplete installation (Kafka inventory not updated) when loading Overview, and provide modal update user interaction
    - fix: Prevents multiple endpoint calls in Alerting User Interface management in Ajax

--- telegraf-kafka/appserver/static/active_button.js
+++ telegraf-kafka/appserver/static/active_button.js
@@ -1,38 +1,41 @@
-require(['splunkjs/mvc/simplexml/ready!'], function(){
-    require(['splunkjs/ready!', 'splunkjs/mvc'], function(mvc){
+require(['splunkjs/mvc/simplexml/ready!'], function() {
+    require(['splunkjs/ready!', 'splunkjs/mvc'], function(mvc) {
 
-                /*
-                        --------------------------------------------------------------
-                        Multi depends buttons - Written by François Toulouse, thanks !
-                        --------------------------------------------------------------
+        /*
+                --------------------------------------------------------------
+                Multi depends buttons - Written by François Toulouse, thanks !
+                --------------------------------------------------------------
 
-                        Usage: Add an html bootstrap button
+                Usage: Add an html bootstrap button
 
-                         <button class="btn" data-token-name="foo" data-token-value="1">Activate foo token</button>
-                         <button class="btn" data-token-name="bar" data-token-value="1">Activate bar token</button>
+                 <button class="btn" data-token-name="foo" data-token-value="1">Activate foo token</button>
+                 <button class="btn" data-token-name="bar" data-token-value="1">Activate bar token</button>
 
 
-                */
+        */
 
-        var defaultTokenModel = mvc.Components.getInstance('default', {create: true});
-        var submittedTokenModel = mvc.Components.getInstance('submitted', {create: true});
+        var defaultTokenModel = mvc.Components.getInstance('default', {
+            create: true
+        });
+        var submittedTokenModel = mvc.Components.getInstance('submitted', {
+            create: true
+        });
 
         function setToken(name, value) {
             defaultTokenModel.set(name, value);
             submittedTokenModel.set(name, value);
         }
 
-                function getToken(name) {
-                        var ret = null;
+        function getToken(name) {
+            var ret = null;
 
-            if(defaultTokenModel.get(name) != undefined){
-                                ret = defaultTokenModel.get(name);
-                        }
-                        else if(submittedTokenModel.get(name) != undefined){
-                                ret = submittedTokenModel.get(name);
-                        }
+            if (defaultTokenModel.get(name) != undefined) {
+                ret = defaultTokenModel.get(name);
+            } else if (submittedTokenModel.get(name) != undefined) {
+                ret = submittedTokenModel.get(name);
+            }
 
-                        return ret;
+            return ret;
         }
 
         function unsetToken(name) {
@@ -40,35 +43,34 @@ require(['splunkjs/mvc/simplexml/ready!'], function(){
             submittedTokenModel.unset(name);
         }
 
-                // For each button with the class "custom-sub-nav"
-                $('.custom-sub-nav').each(function(){
-                        var $btn_group = $(this);
+        // For each button with the class "custom-sub-nav"
+        $('.custom-sub-nav').each(function() {
+            var $btn_group = $(this);
 
-                        /* for each button in this nav:
-                                - Cliking on the button: create the token "data-token-name" with attribute value "data-token-value"
-                                - Button has been clicked already and the user click on it again: removes the token "data-token-name"
-                        */
-                        $btn_group.find('button').on('click', function(){
-                                var $btn = $(this);
-                                var btn_current_label = $btn.html();
-                                var btn_alt_label = $btn.attr('data-alt-label');
-                                var tk_name = $btn.attr('data-token-name');
-                                var tk_value = $btn.attr('data-token-value');
+            /* for each button in this nav:
+                    - Cliking on the button: create the token "data-token-name" with attribute value "data-token-value"
+                    - Button has been clicked already and the user click on it again: removes the token "data-token-name"
+            */
+            $btn_group.find('button').on('click', function() {
+                var $btn = $(this);
+                var btn_current_label = $btn.html();
+                var btn_alt_label = $btn.attr('data-alt-label');
+                var tk_name = $btn.attr('data-token-name');
+                var tk_value = $btn.attr('data-token-value');
 
-                                if( getToken(tk_name) == null){
-                                        setToken(tk_name, tk_value);
-                                        $btn.addClass('active');
-                                }
-                                else{
-                                        unsetToken(tk_name);
-                                        $btn.removeClass('active');
-                                }
+                if (getToken(tk_name) == null) {
+                    setToken(tk_name, tk_value);
+                    $btn.addClass('active');
+                } else {
+                    unsetToken(tk_name);
+                    $btn.removeClass('active');
+                }
 
-                                // Manage button label
-                                $btn.html(btn_alt_label);
-                                $btn.attr('data-alt-label', btn_current_label);
+                // Manage button label
+                $btn.html(btn_alt_label);
+                $btn.attr('data-alt-label', btn_current_label);
 
-                        });
-                });
+            });
         });
+    });
 });
