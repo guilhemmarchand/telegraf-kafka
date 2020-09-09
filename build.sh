98bb1b1781fec9964077ecc0fcc029709b013d66
Guilhem Marchand <guilhem.marchand@gmail.com>
2020-09-09 10:21:58 +0100

    Version 1137 (#59) (#60)
    * Version 1.1.36
    
    - Fix: Avoid mcatalog calls with metric_name as metric_name, some versions of Splunk will incorrectly complain about this, and this causes appinpsect failures in Splunk Base
    
    * Version 1.1.37

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ app="telegraf-kafka"
 version=`grep 'version =' telegraf-kafka/default/app.conf | awk '{print $3}' | sed 's/\.//g'`
 
 rm -f *.tgz
-tar -cvzf ${app}_${version}.tgz --exclude=telegraf-kafka/local --exclude=telegraf-kafka/metadata/local.meta --exclude=telegraf-kafka/lookups/lookup_file_backups telegraf-kafka
+tar -czf ${app}_${version}.tgz --exclude=telegraf-kafka/local --exclude=telegraf-kafka/metadata/local.meta --exclude=telegraf-kafka/lookups/lookup_file_backups telegraf-kafka
 echo "Wrote: ${app}_${version}.tgz"
 
 exit 0
